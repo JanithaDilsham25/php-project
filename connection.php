6a1bc211e7535231f6a36e594160d2622d1109f7
Jdilsham <>
2025-07-04 19:58:53 +0530

    connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = 'x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    $uname = 'jvcgye71sl1jx2fx';
-    $password = 'ohhspp1ch2ngqh5i';
-    $db_name =  'c4mkavc3cfzfr2a5';
+    $servername = 'db';
+    $uname = 'user';
+    $password = 'password';
+    $db_name =  'mydb';
 
     $conn = mysqli_connect($servername, $uname, $password, $db_name);
 
