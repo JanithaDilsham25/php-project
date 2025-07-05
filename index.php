fad09559cfed6ee2c661583562b284ebc388ad23
Jdilsham <>
2025-07-05 14:04:00 +0530

    index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<?php
-// Force HTTPS (if not already using HTTPS)
-if ($_SERVER['HTTPS'] != "on") {
-    header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-    exit();
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
