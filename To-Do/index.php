2d27cf041bf8b12e41631261e37b2457d1908465
Jdilsham <>
2025-07-17 12:14:19 +0530

    index.php

--- To-Do/index.php
+++ To-Do/index.php
@@ -7,7 +7,7 @@ session_start();
 
 // Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
-    header("Location: Login/login.php");
+    header("Location: ../Login/login.php");
     exit();
 }
 
