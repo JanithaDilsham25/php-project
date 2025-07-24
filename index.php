a6277b22efdf1c3dc6d272fc7dc12ff69f8b9423
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-07-24 12:51:01 +0530

    index.php

--- index.php
+++ index.php
@@ -255,7 +255,7 @@ include "connection.php";
   <script src="Project/assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="Project/assets/vendor/purecounter/purecounter_vanilla.js"></script>
   <script src="Project/assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="chatbot/style.css"></script>        
+  <script src="chatbot/script.js"></script>        
 
   <!-- Main JS File -->
   <script src="Project/assets/js/main.js"></script>
