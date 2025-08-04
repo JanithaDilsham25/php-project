c38d77928fadec80b9d2838df91a029201971de1
Jdilsham <154776437+Jdilsham@users.noreply.github.com>
2025-08-04 22:17:50 +0530

    indexx.php

--- chatbot/index.php
+++ chatbot/index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Database Connection</title>
-        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="chatbot/styles/style.css">
         <script src="https://cdn.tailwindcss.com"></script>
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
     </head>
